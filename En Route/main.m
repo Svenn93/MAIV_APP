a11a9b6a6d6e0bac66383c863680514cb35bb58c
Sven Lombaert <sven_93_06@hotmail.com>
2014-06-02 08:36:53 +0200

    Initial Commit

---
+++ En Route/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  En Route
+//
+//  Created by Sven Lombaert on 02/06/14.
+//  Copyright (c) 2014 Sven Lombaert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
